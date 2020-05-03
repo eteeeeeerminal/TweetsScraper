c6abc072d1208ce4586fb3cd7d9b555a2111ff11
eteeeeeerminal <momentneet@gmail.com>
2020-05-03 21:59:38 +0900

    Update scrape_dialogue.py

--- examples/scrape_dialogue.py
+++ examples/scrape_dialogue.py
@@ -3,20 +3,24 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-sys.path.append("../src/TweetsScraper/")
-from TweetsScraper import TweetsScraper
+sys.path.append("../src")
+from Scrapers import TweetsScraper
 
 # キーを取得
 load_dotenv(verbose=True)
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
+i = 1
+while os.path.exists(save_dir:=f"tweets_data{i}"):
+    i += 1
+
 api = TweetsScraper(    consumer_key=os.environ.get("CONSUMER_KEY"),
                         consumer_secret=os.environ.get("CONSUMER_SECRET"),
                         access_token_key=os.environ.get("ACCESS_TOKEN_KEY"),
                         access_token_secret=os.environ.get("ACCESS_TOKEN_SECRET"),
-                        save_dir="tweets_data4"
+                        save_dir=save_dir
                         )
 
-api.get_near_dialogue(user_n=500, dialogue_n=10000)
+api.get_near_dialogue(user_n=500, dialogue_n=100000, save_n=1000)
 api.save()
