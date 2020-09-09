22f226184050dad8058024600582f60a3e95789d
eteeeeeerminal <momentneet@gmail.com>
2020-09-09 16:06:26 +0900

    aozorabunkoapi のラッパー作成

---
+++ src/Scrapers/AozoraBunkoScraper/AozoraBunkoAPI.py
@@ -0,0 +1,63 @@
+import requests
+import os
+
+# 参考 : https://github.com/aozorahack/pubserver2
+# 参考 : https://github.com/aozorahack/aozora-cli
+
+AOZORAPI_HOST = os.environ.get("AOZORAPI_HOST", "www.aozorahack.net")
+AOZORAPI_URL = f"http://{AOZORAPI_HOST}/api/v0.1"
+
+def get_booklist(
+    title=None, author=None, fields=None,
+    limit=None, skip=None,   after=None
+    ) -> requests.Response:
+    params = {
+        'title' : title, 'author' : author,
+        'fields' : fields, 'limit' : limit,
+        'skip' : skip, 'after' : after
+    }
+    params = _shape_params(params)
+    return _aozora_get("books", params=params)
+
+def get_bookinfo(book_id:int) -> requests.Response:
+    return _aozora_get(f"books/{book_id}")
+
+def get_bookcard(book_id:int) -> requests.Response:
+    return _aozora_get(f"books/{book_id}/card")
+
+def get_booktxt(book_id:int) -> requests.Response:
+    return _aozora_get(f"books/{book_id}/content?format=txt")
+
+def get_bookhtml(book_id:int) -> requests.Response:
+    return _aozora_get(f"books/{book_id}/content?format=html")
+
+def get_personlist(name=None) -> requests.Response:
+    params = _shape_params({'name' : name})
+    return _aozora_get("persons", params=params)
+
+def get_personinfo(person_id:int) -> requests.Response:
+    return _aozora_get(f"persons/{person_id}")
+
+def get_workerlist(name=None) -> requests.Response:
+    params = _shape_params({'name' : name})
+    return _aozora_get("workers", params=params)
+
+def get_workerinfo(worker_id:int) -> requests.Response:
+    return _aozora_get(f"workers/{worker_id}")
+
+def get_ranking(
+    year, month, data_type="txt"
+    ) -> requests.Response:
+    return _aozora_get(f"ranking/{data_type}/{year}/{month:0>2d}")
+
+def _aozora_get(path:str, params={}) -> requests.Response:
+    return requests.get(f"{AOZORAPI_URL}/{path}", params=params)
+
+def _shape_params(params:dict) -> dict:
+    return {
+        key:value for (key, value) in params.items()
+        if value is not None
+    }
+
+if __name__ == "__main__":
+    print(get_booktxt(2093).text)
---
+++ src/Scrapers/AozoraBunkoScraper/__init__.py
@@ -0,0 +1,5 @@
+from .AozoraBunkoAPI import (
+    get_booklist, get_bookcard, get_bookinfo, get_bookhtml, get_booktxt,
+    get_personlist, get_personinfo, get_workerlist, get_workerinfo,
+    get_ranking
+)
